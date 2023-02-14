07ff91d12e35b0f159ead1f0fce4c80f39cba44c
GerardoITA <gerardo.giusti@tiscali.it>
2023-02-14 03:31:11 +0100

    add_foreign_keys aggiunto

---
+++ database/migrations/9999_02_14_021650_add_foreign_keys.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('posts', function(Blueprint $table){
+
+            $table  -> bigInteger('user_id') -> unsigned();
+
+            $table  -> foreign('user_id')
+                    -> references('id')
+                    -> on('users');
+        });
+        Schema::table('person_details', function (Blueprint $table) {
+
+            $table->bigInteger('user_id')->unsigned();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
